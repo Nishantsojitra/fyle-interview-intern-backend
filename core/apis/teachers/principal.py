3e9720bb2836b9a2a154854f80da4ddae84f9d2c
Nishantsojitra <nishantsojitra8@gmail.com>
2024-07-19 18:08:22 +0530

    ok

--- core/apis/teachers/principal.py
+++ core/apis/teachers/principal.py
@@ -5,7 +5,6 @@ from core.models import users
 principal_blueprint = Blueprint('principal', __name__)
 
 @principal_blueprint.route('/principal/teachers', methods=['GET'])
-@authenticate_principal
 def get_principal_teachers():
     teachers = users.query.filter_by(role='teacher').all()
     return jsonify({'data': [teacher.to_dict() for teacher in teachers]})
